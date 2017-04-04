350870cd5c5ef41d1a6b815cc25824ef4b2ebdfe
Anne-Laure VACHER <avacher@student.42.fr>
2017-04-04 17:33:32 +0200

    setenv -- OK export -- BUG

--- srcs/sh_bi_handle_env_modif.c
+++ srcs/sh_bi_handle_env_modif.c
@@ -9,11 +9,17 @@ static int			add_env(char *name, char *value, int local, t_duo **env)
 
 	t_duo 				*tmp;
 
+	if (local == TRUE)
+	{printf("************local\n");
+		*env = savior_local(NULL, FALSE);
+	}
+	else if (local == FALSE)
+	{printf("****************env\n");
+		*env = savior_env(NULL, FALSE);
+	}
 	tmp = *env;
 	if (name == NULL)
 		return (sh_error(TRUE, 26, "setenv", NULL));
-	while (tmp)
-		tmp = tmp->next;
 	if (value && value[0] == 26)
 		duo_pushback(&tmp, name, "");
 	else
@@ -34,38 +40,42 @@ int					change_env(char *name, char *value, int local)
 	if (DEBUG_BI == 1)
 		ft_putendl_fd("----------------------- CHANGE ENV ------------------", 2);
 
+	t_duo				*tmp;
 	t_duo				*env;
-	t_duo				*head;
 
-	env = NULL;
-	printf("DANS CHANGE ENV {{{{{{%d}}}}}}\n", local);
+	tmp = NULL;
+	printf("DANS CHANGE tmp {{{{{{%d}}}}}} ((%s)(%s))\n", local, name, value);
 	if (local == TRUE)
-		env = savior_local(NULL, FALSE);
+	{printf("************local\n");
+		tmp = savior_local(NULL, FALSE);
+	}
 	else if (local == FALSE)
-		env = savior_env(NULL, FALSE);
-	head = env;
-	while (env)
+	{printf("****************tmp\n");
+		tmp = savior_env(NULL, FALSE);
+	}
+	env = tmp;
+	while (tmp)
 	{
-	//printf("toto trololo -- 1 env ((%s)) name ((%s))\n", env->name, name);
-		if (env && name && ft_strcmp(env->name, name) == 0)
+	//printf("toto trololo -- 1 tmp ((%s)) name ((%s))\n", tmp->name, name);
+		if (tmp && name && ft_strcmp(tmp->name, name) == 0)
 		{
-	//printf("toto trololo -- 2\n");
-			ft_strdel(&(env->value));
-			if (value && value[0] != 26 && !(env->value = ft_strdup(value)))
+	printf("toto trololo -- 2\n");
+			ft_strdel(&(tmp->value));
+			if (value && value[0] != 26 && !(tmp->value = ft_strdup(value)))
 				return (sh_error(FALSE, 6, NULL, NULL));
 			return (TRUE);
 		}
 	//printf("toto trololo -- 3\n");
-		env = env->next;
+		tmp = tmp->next;
 	//printf("toto trololo -- 4\n");
 	}
-	add_env(name, value, local, &head);
+	add_env(name, value, local, &env);
 
 
-	//t_duo *toto = head;
+	//t_duo *toto = env;
 	//while (toto)
 	//{
-	//printf("name ap add env [[%s]]\n", toto->name);
+	//printf("name ap add tmp [[%s]]\n", toto->name);
 	//toto = toto->next;
 	//}
 
