9ea0a6e7da13e2af608d5f70522e561614f18d1f
nkhaldi <nkhaldi@student.42.fr>
2017-04-13 18:34:43 +0200

    norme fini ok

--- libft/srcs/ft_strdel.c
+++ libft/srcs/ft_strdel.c
@@ -6,14 +6,11 @@
 /*   By: avacher <avacher@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/25 16:33:50 by avacher           #+#    #+#             */
-/*   Updated: 2015/11/30 14:24:24 by avacher          ###   ########.fr       */
+/*   Updated: 2017/04/13 18:34:09 by nkhaldi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-
-#include<stdio.h>
 
 void		ft_strdel(char **as)
 {
